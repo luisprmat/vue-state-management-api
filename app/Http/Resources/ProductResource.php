256b4e3232b2e56211782cf2bbdacf69cefc22ff
Luis Parrado <luisprmat@gmail.com>
2026-01-09 10:04:31 -0500

    Add routes + controller  + resource to Products

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'price' => (float) $this->price,
+            'use_for_upselling' => $this->use_for_upselling,
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('products', [ProductController::class, 'index']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
