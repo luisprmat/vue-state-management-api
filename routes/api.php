cff14048d5f3299e0b0fbad39d7bb3f31a736da7
Luis Parrado <luisprmat@gmail.com>
2026-01-12 09:41:07 -0500

    Move api user to controller class

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProductController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('products', [ProductController::class, 'index']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [UserController::class, 'show']);
+});
